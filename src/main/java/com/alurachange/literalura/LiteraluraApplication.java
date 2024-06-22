4cccd54dddd7e6e1944ff9351cf0f8db35fa7a23
Danjork <40806603+Danjork@users.noreply.github.com>
2024-06-22 19:41:06 -0400

    funciona menu

--- src/main/java/com/alurachange/literalura/LiteraluraApplication.java
+++ src/main/java/com/alurachange/literalura/LiteraluraApplication.java
@@ -3,15 +3,18 @@ package com.alurachange.literalura;
 import com.alurachange.literalura.principal.Principal;
 import com.alurachange.literalura.service.IAutor;
 import com.alurachange.literalura.service.ILibro;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
-
+	
+	@Autowired
 	private ILibro libroRepository;
 
+	@Autowired
 	private IAutor autorRepository;
 
 	public static void main(String[] args) {
--- src/main/java/com/alurachange/literalura/model/Autor.java
+++ src/main/java/com/alurachange/literalura/model/Autor.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Entity
 public class Autor {
 
     @Id
